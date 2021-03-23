7ee6e748f556c5bdfa213f0ec1070bb34f8d09c6
MFella <czarusiek112@wp.pl>
2021-03-23 01:04:38 +0100

    auth on back init, logos added

--- Chatterbox.API/src/app.module.ts
+++ Chatterbox.API/src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot()],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
